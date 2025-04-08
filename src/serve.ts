3446286440294812bdcdb3d76ac7e51e301ca0bb
Eduardo A. Pacheco <epacheco.dev@gmail.com>
2025-04-08 14:49:13 -0300

    feat: enable CORS for all origins in API

--- src/serve.ts
+++ src/serve.ts
@@ -2,6 +2,7 @@ import 'tsconfig-paths/register';
 import express from 'express';
 import router from '@/router';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config({
   path: '../.env',
@@ -10,9 +11,17 @@ dotenv.config({
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['*'],
+  }),
+);
+
 app.use(express.json());
 
-app.use('/api', router);
+app.use('/api/v1', router);
 
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
